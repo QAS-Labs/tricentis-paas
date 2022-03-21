15e6d9e2ffbcb7ec7145c3bb7f80948cb53dcb6b
Liem Nguyen <l.nguyen@tricentis.com>
2022-03-21 08:15:07 -0700

    Updated cluster to 1.23

--- kindup.sh
+++ kindup.sh
@@ -14,7 +14,7 @@ fi
 
 # create a cluster with the local registry enabled in containerd
 echo "Creating cluster..."
-cat <<EOF | kind create cluster --config=-
+cat <<EOF | kind create cluster --image kindest/node:v1.23.0 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
